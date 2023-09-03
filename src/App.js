956a68a289b498457da011ad2c5d03254eca3e14
Ahmed Abo Zaid <aabozaid0094@gmail.com>
2023-09-03 18:41:33 +0300

    Convert app component to be class based component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import './App.css';
+import { Component } from 'react';
 import Users from './components/Users';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-      <div className="App">
-          <Header></Header>
-          <Users></Users>
-          <Footer></Footer>
-      </div>
-  );
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <Header></Header>
+                <Users></Users>
+                <Footer></Footer>
+            </div>
+        );
+    }
 }
 
 export default App;
