9dd0cece41ba2c76213f77b73dbdccd3bef9859f
Ahmed Abo Zaid <aabozaid0094@gmail.com>
2023-09-03 19:02:26 +0300

    Inject column into user component to let key be effective

--- src/components/User.js
+++ src/components/User.js
@@ -1,17 +1,23 @@
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
 
 const User = (props) => {
-    const { name, phone, website } = props;
+    const { name, phone, website, company, address} = props;
     return (
-        <Card className="User">
-            <Card.Body>
-                <Card.Title>{name}</Card.Title>
-                <Card.Text>
-                    <div>{phone}</div>
-                    <div>{website}</div>
-                </Card.Text>
-            </Card.Body>
-        </Card>
+        <Col xxl="3" lg="4" sm="6" xs="12" className="my-3">
+            <Card className="User">
+                <Card.Body>
+                    <Card.Title>{name}</Card.Title>
+                    <Card.Text>
+                        <span className="d-flex">{company.name}</span>
+                        <span className="d-flex">{phone}</span>
+                    </Card.Text>
+                    <Button variant="secondary" className="me-2 my-2">{website}</Button>
+                    <Button variant="primary" className="me-2 my-2" target="_blank" href={"https://www.google.com/maps/search/?api=1&query=" + address.geo.lat + "," + address.geo.lng}>Map Address</Button>
+                </Card.Body>
+            </Card>
+        </Col>
     );
 };
 
--- src/components/Users.js
+++ src/components/Users.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react';
 import User from './User';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 
 const Users = () => {
     const [users, setUsers] = useState([]);
@@ -10,7 +9,6 @@ const Users = () => {
         fetch("https://jsonplaceholder.typicode.com/users")
             .then((response) => response.json())
             .then((data) => {
-                // console.log(data);
                 setUsers(data);
             })
             .catch((err) => {
@@ -20,8 +18,8 @@ const Users = () => {
 
     return (
         <Container className="Users">
-            <Row>
-                {users.map((u)=><Col xxl="3" lg="4" sm="6" xs="12" className="my-3"><User key={u.id} {...u}></User></Col>)}
+            <Row className="justify-content-center">
+                {users.map((u)=><User key={u.id} {...u}></User>)}
             </Row>
         </Container>
     );
