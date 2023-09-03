421cc361a899d301f543823fb8eb04f1dcaf0604
Ahmed Abo Zaid <aabozaid0094@gmail.com>
2023-09-03 18:32:21 +0300

    Convert user to be as card layout

--- src/components/User.js
+++ src/components/User.js
@@ -1,11 +1,18 @@
+import Card from 'react-bootstrap/Card';
+
 const User = (props) => {
-    const {name, phone, website} = props;
+    const { name, phone, website } = props;
     return (
-        <div className="User">
-            <div>{name}</div>
-            <div>{phone}</div>
-            <div>{website}</div>
-        </div>
+        <Card className="User">
+            <Card.Body>
+                <Card.Title>{name}</Card.Title>
+                <Card.Text>
+                    <div>{phone}</div>
+                    <div>{website}</div>
+                </Card.Text>
+            </Card.Body>
+        </Card>
     );
 };
+
 export default User;
